23aafb912fbd7132554c841dc977f1ec998e4bc7
AlexandreN7 <prxo@live.fr>
2015-07-16 11:04:05 +0200

    nettoyage / amelioration de asserv

--- app/capture/asserv.cpp
+++ app/capture/asserv.cpp
@@ -18,46 +18,56 @@ extern int asserv_index;
 extern int dicho_value;
 extern int order_value;
 
+void uart_config(){
+    serialib LS;                                                            // Object of the serialib class
+    int Ret;                                                                // Used for return values
+    char Buffer[128];
+
+    // Open serial port
+
+    Ret=LS.Open(DEVICE_PORT,9600);                                        // Open serial link at 115200 bauds
+    if (Ret!=1) {                                                           // If an error occured...
+        printf ("Error while opening port. Permission problem ?\n");        // ... display a message ...
+        //return Ret;                                                         // ... quit the application
+    }
+    printf ("Serial port opened successfully !\n");
+    // Read a string from the serial device
+    //Ret=LS.ReadString(Buffer,'\n',128,1000);                                // Read a maximum of 128 characters with a timeout of 5 seconds                                                                        // The final character of the string must be a line feed ('\n'
+    Ret=LS.WriteString("Yolo\n");
+
+    // Close the connection with the device
+
+    LS.Close();
+
+    //return 0;
+
+}
+
+
+void spi_config(){}
+void pwm_config(){}
+void manual_config(){}
+
+
+
 void Asserv_thread::run()
 {
     switch(asserv_index){
         case 0 :
+        qDebug("uart_config");
+        uart_config();
         break;
         case 1 :
+        spi_config();
         break;
         case 2 :
+        pwm_config();
         break;
         case 3 :
+        manual_config();
         break;
     }
-        serialib LS;                                                            // Object of the serialib class
-        int Ret;                                                                // Used for return values
-        char Buffer[128];
 
-
-        // Open serial port
-
-        Ret=LS.Open(DEVICE_PORT,9600);                                        // Open serial link at 115200 bauds
-        if (Ret!=1) {                                                           // If an error occured...
-            printf ("Error while opening port. Permission problem ?\n");        // ... display a message ...
-            //return Ret;                                                         // ... quit the application
-        }
-        printf ("Serial port opened successfully !\n");
-
-
-
-        // Read a string from the serial device
-        //Ret=LS.ReadString(Buffer,'\n',128,1000);                                // Read a maximum of 128 characters with a timeout of 5 seconds
-
-                                                                                // The final character of the string must be a line feed ('\n')
-
-        Ret=LS.WriteString("Yolo\n");
-
-        // Close the connection with the device
-
-        LS.Close();
-
-        //return 0;
 }
 
 
